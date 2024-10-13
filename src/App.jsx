677316bd649119fe29c1599c33a7a2cb1d192ca3
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-10-13 23:33:22 +0530

    status filter

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,37 @@ import bgLight from "./assets/images/bg-desktop-light.jpg";
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("all");
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
 
+  const toggleComplete = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].status =
+      newTasks[index].status === "active" ? "completed" : "active";
+    setTasks(newTasks);
+  };
+
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && e.target.value.trim() !== "") {
       const newTask = { title: e.target.value, status: "active" };
       setTasks([...tasks, newTask]);
       e.target.value = "";
     }
+  };
+
+  const filterTasks = (status) => {
+    return tasks.filter((task) => task.status === status);
   }
+
+  const clearCompleted = () => {
+    const newTasks = tasks.filter((task) => task.status === "active");
+    setTasks(newTasks);
+  };
+
+  const itemsLeft = tasks.filter((task) => task.status === "active").length;
 
   return (
     <>
@@ -50,7 +69,7 @@ const App = () => {
               T O D O{" "}
             </h1>
             <button
-              className={` w-6 h-7 mr-2  rounded ${
+              className={` w-6 h-[26px] rounded ${
                 isDarkMode
                   ? "bg-[url('./assets/images/icon-sun.svg')]"
                   : "bg-[url('./assets/images/icon-moon.svg')]"
@@ -58,10 +77,10 @@ const App = () => {
               onClick={toggleTheme}
             ></button>
           </header>
-          <main className="p-4">
+          <main className="mt-4">
             <input
               type="text"
-              className={`h-10 w-full pl-10 ${
+              className={`h-10 w-full pl-10 rounded-md ${
                 isDarkMode ? "bg-black" : "bg-white"
               }`}
               name="newTask"
@@ -70,11 +89,30 @@ const App = () => {
               onKeyDown={handleKeyDown}
             />
             <div className="mt-4">
-              {tasks.map((task, index) => (
-                <Task key={index} status={task.status} title={task.title} isDarkMode={isDarkMode} />
+              {tasks.filter((task) => (task.status == filter) || (filter == 'all') ).map((task, index) => (
+                <Task
+                  key={index}
+                  index={index}
+                  status={task.status}
+                  title={task.title}
+                  isDarkMode={isDarkMode}
+                  toggleComplete={toggleComplete}
+                />
               ))}
+              <div className="flex justify-between items-center">
+                <div>
+                  <a href="#">{itemsLeft} Items left</a>
+                </div>
+                <div className="flex justify-center items-center gap-2">
+                  <a href="#" onClick={() => setFilter('all')}>All</a>
+                  <a href="#" onClick={() => setFilter('active')}>Active</a>
+                  <a href="#" onClick={() => setFilter('completed')}>Completed</a>
+                </div>
+                <div>
+                  <a href="#" onClick={clearCompleted}>Clear completed</a>
+                </div>
+              </div>
             </div>
-
           </main>
         </div>
       </div>
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,9 +1,9 @@
-export default function Task({ status, title, isDarkMode }) {
+export default function Task({ index, status, title, isDarkMode, toggleComplete }) {
   return (
     <>
-      <div className={`flex justify-start items-center border-b-[1px] border-b-neutral-lightGrayishBlue shadow-2xl ${isDarkMode ? 'bg-neutral-veryDarkGrayishBlue': 'bg-white' }`}>
-        <input className={`size-10 rounded-full`} type="checkbox" name="state" id="state" />
-        <span className={`text-xl ${isDarkMode ? "text-white" : "text-black"}`}>
+      <div className={`flex justify-start items-center border-b-[1px] border-b-neutral-lightGrayishBlue ${isDarkMode ? 'bg-neutral-veryDarkGrayishBlue': 'bg-white' }`}>
+        <input className={`size-10 rounded-full`} type="checkbox" name="state" id="state" onChange={() => toggleComplete(index)}/>
+        <span className={`text-xl ${isDarkMode ? "text-white" : "text-black"} ${status === 'completed' ? 'line-through' : ''}`}>
           {" "}
           {title}{" "}
         </span>
